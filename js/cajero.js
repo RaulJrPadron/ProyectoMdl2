3f382a9c9c8a8de61984f3b1c37b22deb70c10be
Raul Padron <padron.jr@outlook.com>
2022-05-18 13:36:08 -0500

    Se agregó la función del btn Retirar, se comentaron algunas línea inecesarias y se agregó la función de esuchar evento input de la caja de texto donde coloco la cantidad para que no haga nada hasta que presione el btn consultar o retirar.

--- js/cajero.js
+++ js/cajero.js
@@ -19,8 +19,10 @@ function datosUsuario(){
     // console.log(nombre)
     // console.log(saldo)
     // console.log(datosUsuario.imagen)
+    escuchaEventoInput();
     escuchaEventoConsultar(datosUsuario);
     escuchaEventoIngresar(datosUsuario);
+    escuchaEventoRetirar(datosUsuario);
 }
 
 const escuchaEventoConsultar = (datosUsuario) => {
@@ -37,7 +39,7 @@ const escuchaEventoConsultar = (datosUsuario) => {
 };
 
 const escuchaEventoIngresar = (datosUsuario) => {
-    console.log(datosUsuario.saldo)
+    // console.log(datosUsuario.saldo)
     const elementoPadre = document.getElementById("ElementoPadre");
     // var InputNumber = document.querySelector('InputNumber');
     // var res = saldo + InputNumber;
@@ -56,5 +58,35 @@ const escuchaEventoIngresar = (datosUsuario) => {
             }));
         };
         datosUsuario();
+    });
+}
+
+const escuchaEventoRetirar = (datosUsuario) => {
+    // console.log(datosUsuario.saldo)
+    const elementoPadre = document.getElementById("ElementoPadre");
+    elementoPadre.addEventListener("click", (event) => {
+        console.log("click")
+        if(event.target.tagName === "BUTTON" && event.target.classList.contains("btn-danger")){
+            
+            var InputNumber = document.getElementById('InputNumber').value;
+            console.log(InputNumber)
+            var resta = parseInt(datosUsuario.saldo) - parseInt(InputNumber)
+            localStorage.setItem('usuario', JSON.stringify({
+                ...datosUsuario,
+                saldo: resta,
+            }));
+        };
+        datosUsuario();
+    });
+}
+
+const escuchaEventoInput = () => {
+    const numberInput = document.getElementById('InputNumber')
+    numberInput.addEventListener("keydown", (evento) => {
+        if (evento.key == "Enter") {
+            // Prevenir
+            evento.preventDefault();
+            return false;
+        }
     });
 }
