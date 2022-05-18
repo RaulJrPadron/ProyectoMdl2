b3141c9f1b6ddc789e403c84f59f23b8be202c41
Raul Padron <padron.jr@outlook.com>
2022-05-18 17:02:01 -0500

    Funcionalidad terminada le faltan detalles estéticos

--- js/cajero.js
+++ js/cajero.js
@@ -7,22 +7,22 @@ window.addEventListener('load', function() {
 })
 
 function datosUsuario(){
-    const datosUsuario = JSON.parse(localStorage.getItem('usuario'));
+    const datoUsuario = JSON.parse(localStorage.getItem('usuario'));
     const id = document.querySelector('#datosUsuarioId');
     const nombre = document.querySelector('#datosUsuarioNombre');
     const saldo = document.querySelector('#datosUsuarioSaldo');
     const imagen = document.querySelector('#datosUsuarioImagen');
-    nombre.innerHTML = datosUsuario.nombre;
+    nombre.innerHTML = datoUsuario.nombre;
     // saldo.innerHTML = datosUsuario.saldo;
-    imagen.src = datosUsuario.imagen
+    imagen.src = datoUsuario.imagen
     // console.log(datosUsuario)
     // console.log(nombre)
     // console.log(saldo)
     // console.log(datosUsuario.imagen)
     escuchaEventoInput();
-    escuchaEventoConsultar(datosUsuario);
-    escuchaEventoIngresar(datosUsuario);
-    escuchaEventoRetirar(datosUsuario);
+    escuchaEventoConsultar(datoUsuario);
+    escuchaEventoIngresar(datoUsuario);
+    escuchaEventoRetirar(datoUsuario);
 }
 
 const escuchaEventoConsultar = (datosUsuario) => {
@@ -38,45 +38,80 @@ const escuchaEventoConsultar = (datosUsuario) => {
     });
 };
 
-const escuchaEventoIngresar = (datosUsuario) => {
-    // console.log(datosUsuario.saldo)
+const escuchaEventoIngresar = (datoUsuario) => {
     const elementoPadre = document.getElementById("ElementoPadre");
-    // var InputNumber = document.querySelector('InputNumber');
-    // var res = saldo + InputNumber;
-    // console.log(res)
     elementoPadre.addEventListener("click", (event) => {
-        // event.preventDefault();
-        console.log("click")
+        // event.preventDefault(); //Se comento porque había un ERROR - Sesión Sensei Alejandro
         if(event.target.tagName === "BUTTON" && event.target.classList.contains("btn-success")){
-            
-            var InputNumber = document.getElementById('InputNumber').value;
-            console.log(InputNumber)
-            var suma = parseInt(datosUsuario.saldo) + parseInt(InputNumber)
-            localStorage.setItem('usuario', JSON.stringify({
-                ...datosUsuario,
-                saldo: suma,
-            }));
+            var InputNumber = document.getElementById('InputNumber');
+            // console.log("Input",InputNumber.value)
+            if(InputNumber.value == null || InputNumber.value == ''){
+                alert("Debe ingresar un número para continuar");
+            }else{
+                // console.log(InputNumber)
+                if((parseInt(datoUsuario.saldo) + parseInt(InputNumber.value)) < 991){
+                    localStorage.setItem('usuario', JSON.stringify({
+                        ...datoUsuario,
+                        saldo: (parseInt(datoUsuario.saldo) + parseInt(InputNumber.value))
+                    }));
+                    datosUsuario()
+                    event.preventDefault()
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Monto ingresado correctamente. Consulte saldo',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                    // alert("Monto ingresado correctamente. Consulte saldo")
+                }else{
+                    event.preventDefault()
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Excede el límite. Ingrese otra cantidad.'
+                    });
+                    // alert("Excede el límite. Ingrese otra cantidad.");
+                }
+            }
         };
-        datosUsuario();
     });
 }
 
-const escuchaEventoRetirar = (datosUsuario) => {
-    // console.log(datosUsuario.saldo)
+const escuchaEventoRetirar = (datoUsuario) => {
     const elementoPadre = document.getElementById("ElementoPadre");
     elementoPadre.addEventListener("click", (event) => {
         console.log("click")
-        if(event.target.tagName === "BUTTON" && event.target.classList.contains("btn-danger")){
-            
-            var InputNumber = document.getElementById('InputNumber').value;
-            console.log(InputNumber)
-            var resta = parseInt(datosUsuario.saldo) - parseInt(InputNumber)
-            localStorage.setItem('usuario', JSON.stringify({
-                ...datosUsuario,
-                saldo: resta,
-            }));
-        };
-        datosUsuario();
+        if(event.target.tagName === "BUTTON" && event.target.classList.contains("btn-danger")){ 
+            var InputNumber = document.getElementById('InputNumber');
+            if(InputNumber.value == null || InputNumber.value == ''){
+                alert("Debe ingresar un número para continuar")
+            }else{
+                var resta = parseInt(datoUsuario.saldo) - parseInt(InputNumber.value)
+                if(resta > 9)
+                {
+                    localStorage.setItem('usuario', JSON.stringify({
+                        ...datoUsuario,
+                        saldo: resta,
+                    }));
+                    datosUsuario()
+                    event.preventDefault()
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Retiro Existoso. Consulte saldo',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                    // alert("Retiro Existoso. Consulte saldo")
+                }else{
+                    event.preventDefault()
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Su cuente debe tener un mínimo de $10.'
+                    });
+                }
+            }  
+        }; 
     });
 }
 
